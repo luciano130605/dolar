a6387d031aba0181bb0fc3f5f83448f094a3f829
luciano <luciano.delapena@gmail.com>
2026-06-09 11:06:00 -0300

    fix server.js vercel

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
 const cors = require('cors')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const HOSTNAME = '127.0.0.1'
-const PORT = 3100
+
 
 const routerPrecio = require('./router/precio.router')
 app.use('/', routerPrecio)
@@ -16,8 +14,5 @@ app.get('/', (req, res) => {
     res.status(200).send('Servidor corriendo')
 })
 
-app.listen(PORT, HOSTNAME, (req, res) => {
-    console.log(`Servidor corriendo en http://${HOSTNAME}:${PORT}`)
-})
 
 module.exports = app
